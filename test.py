aedee0244345a4c5aa7e53f09ce8749b947be0a8
Ben Tristem <github@bentristem.com>
2018-05-06 20:03:02 +0100

    Pycharm IDE setup and refactoring tested

--- test.py
+++ test.py
@@ -1,17 +1,28 @@
 import sys, pygame
 
+
 def play_game():
     pygame.init()
-    size = width, height = 320, 240
+    size = 320, 240
     screen = pygame.display.set_mode(size)
     black = 0, 0, 0
 
+    display_loading_screen(black, screen)
+
     while True:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-          
-      screen.fill(black)
-      pygame.display.flip()
+        respond_to_quit()
+
+
+def display_loading_screen(black, screen):
+    screen.fill(black)
+    pygame.display.flip()
+
+
+def respond_to_quit():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
 
 if __name__ == "__main__":
-  play_game()
+    play_game()
